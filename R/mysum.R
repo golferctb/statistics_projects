c9c83f63af560f55abf38f31e18996a24caf9025
golferctb <golferctb@gmail.com>
2025-09-14 15:13:27 -0500

    First commit!

---
+++ R/mysum.R
@@ -0,0 +1,13 @@
+#' @title Quadratic function
+#'
+#' @param x input vector
+#'
+#' @returns squared vector values
+#' @export
+#'
+#' @examples
+#' mydf <- data.frame(Name = c("Charlie", "Alice"), Age = c(12, 15), Gender = c("M", "F"))
+#' mysum(mydf)
+mysum <- function(x) {
+  x^2
+}
