58b1997d7fe98c0b81bebc46f3fb043f3a6d6d33
golferctb <golferctb@gmail.com>
2025-10-17 13:27:20 -0500

    Pushing recent changes

---
+++ R/timesample.R
@@ -0,0 +1,27 @@
+#' Timed Sample Charts
+#'
+#' @param n A vector with a sample
+#' @param iter number of bar charts to make
+#' @param time Time (in seconds) between each bar to be displayed.
+#'
+#' @returns Generates a new bar chart to console every specified second.
+#' @export
+#'
+#' @examples
+#' timesample(100, 10, 1)
+timesample=function(n, iter=10,time=0.5){
+  for( i in 1:iter){
+    #make a sample
+    s=sample(1:10,n,replace=TRUE)
+    # turn the sample into a factor
+    sf=factor(s,levels=1:10)
+    #make a barplot
+    barplot(table(sf)/n,beside=TRUE,col=rainbow(10),
+            main=paste("Example sample()", " iteration ", i, " n= ", n,sep="") ,
+            ylim=c(0,0.2)
+    )
+
+    #release the table
+    Sys.sleep(time)
+  }
+}
